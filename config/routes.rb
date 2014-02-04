8cef0c0d101e3a172007d176118d99fe80837f9f
trrr <onegalgo@gmail.com>
2014-02-05 00:41:43 +0400

    Deleted devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 ReplaceIt::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
-  devise_for :users
-
   mount Resque::Server.new, :at => "/resque"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
