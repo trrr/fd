11641ac51e4516fdc336e293d10004d1ba848dcb
trrr <onegalgo@gmail.com>
2014-02-05 18:16:17 +0400

    remove unneeded comment

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,3 @@
 class Category < ActiveRecord::Base
   has_many :authors
-
-  # if author has many categories
-  # has_many :authors_categories
-  # has_many :authors, through: :authors_categories
 end
