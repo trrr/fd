61cf1125cc29615e0401bb4d01452b61018ce45b
trrr <onegalgo@gmail.com>
2014-02-05 13:16:05 +0400

    Added category_id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140204075642) do
+ActiveRecord::Schema.define(version: 20140205084822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20140204075642) do
     t.integer  "author_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
   add_index "posts", ["author_id"], name: "index_posts_on_author_id", using: :btree
