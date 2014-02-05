d7311958bd6bdeb3e604e5758033e470532ac08f
trrr <onegalgo@gmail.com>
2014-02-05 12:46:40 +0400

    Started with API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  def tags 
+    @tags = Category.all
+    render json: @tags.as_json
+  end
+
+  def posts
+    @posts = Post.page(page) unless params[:tag_id]
+    # @posts = Post.where("")
+    page = params[:page] || 1
+    per_page = params[:per_page] || 10
+    up_or_down = params['action']['behavior'] || 'up'
+
+    
+    render json: @posts
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ReplaceIt::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   mount Resque::Server.new, :at => "/resque"
+
+  get '/tags' => 'application#tags'
+  get '/posts' => 'application#posts'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
