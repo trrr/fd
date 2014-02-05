2c27fedc09b9b9537ec446eb6552bf0483109e94
trrr <onegalgo@gmail.com>
2014-02-05 13:15:41 +0400

    Added temp before_save hook

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ActiveRecord::Base
-  default_scope order('updated_time DESC')
+  default_scope { order('updated_time DESC') }
+  before_save :set_category
   belongs_to :author
+
+  private
+    def set_category
+      self.category_id = self.author.category_id
+    end
 end
