9cd8c3752cf898defd5087f8e8660cba4686bfd3
trrr <onegalgo@gmail.com>
2014-02-06 23:27:05 +0400

    Changed the timepoint handling. Saved one db call.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
 
+  # I still don't like this very much. Mostly the fact what we have to perform
+  # search with dummy data when no parameters are given. Will think of it later.
+
   def index
     @posts = Post.get_posts_by_params(categories_ids, params[:page], params[:per_page], timepoint, up_or_down)
     render json: @posts.as_json(only: [:id, :message, :picture, :updated_time, :category_id, :author_id])
@@ -8,12 +11,11 @@ class PostsController < ApplicationController
   private
 
     def timepoint
-      given_id = params[:action][:id] || Post.last.id
-      Post.find(given_id).updated_time
+      params['action']['id'] ? Post.find(params['action']['id']).updated_time : "2000-01-01T00:00:00.000Z"
     end
 
     def up_or_down
-      params[:action][:behavior] == 'down' ? '<' : '>'
+      params['action']['behavior'] == 'down' ? '<' : '>'
     end
 
     def categories_ids
