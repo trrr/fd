c9b1145b47f0379635fd37a6b6e81ac3a2f9aa59
trrr <onegalgo@gmail.com>
2014-02-06 23:09:43 +0400

    Refactored controller to be less ugly

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,22 @@
 class PostsController < ApplicationController
 
-# This is seriously ugly. 
-# Basically what this crap does is sets defaults to passed arguments
-# and asks Model for posts what fit given criteria.
-
   def index
-    # Setting defaults.
-    categories_ids = params[:tag_id] || Category.all.map(&:id)
-    page = params[:page] || 1
-    per_page = params[:per_page] || 10
-    from_id = params['action']['id'] || Post.last.id
-    timepoint = Post.find(from_id).updated_time
-
-    
-    #Fetching post from db. TODO: Refactor this ASAP!
-    @posts = if params['action']['behavior'] == 'down'
-                      Post.get_previous(categories_ids, page, per_page, timepoint)
-                    else
-                      Post.get_next(categories_ids, page, per_page, timepoint)
-                    end
-
-    # sending posts to user
+    @posts = Post.get_posts_by_params(categories_ids, params[:page], params[:per_page], timepoint, up_or_down)
     render json: @posts.as_json(only: [:id, :message, :picture, :updated_time, :category_id, :author_id])
   end
+
+  private
+
+    def timepoint
+      given_id = params['id'] || Post.last.id
+      Post.find(given_id).updated_time
+    end
+
+    def up_or_down
+      params['behavior'] == 'down' ? '<' : '>'
+    end
+
+    def categories_ids
+      params[:tag_id] || Category.all.map(&:id)
+    end
 end
