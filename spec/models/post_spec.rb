329e22c9434749070c65f82e1043ee7e82831877
trrr <onegalgo@gmail.com>
2014-02-06 23:39:32 +0400

    started testing, but have to stub the network first

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -20,11 +20,25 @@ describe Post do
     # expect(Post.all.map(&:post_id)).to eq p.map(&:post_id)
   end
 
-  it "gives previous posts" do
-    pending "TODO"
-  end
+ describe "retrieving posts" do
+    let(:timepoint) {"2000-01-01T00:00:00.000Z"}
+    let(:up_or_down) {'>'}
+    let(:categories_ids) {Category.all.map(&:id)}
+    let(:page) {1}
+    let(:per_page) {10}
 
-  it "gives next posts" do
-    pending "TODO"
+    it "returns given amount of posts" do
+      pending "can't test without stubing the network!"
+      # expect(Post.get_posts_by_params(categories_ids, page, 5, timepoint, up_or_down).count).to eq 5
+    end
+
+    it "gives previous posts" do
+      
+    end
+
+    it "gives next posts" do
+      pending "TODO"
+    end
+
   end
 end
