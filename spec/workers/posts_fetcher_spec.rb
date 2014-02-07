8628e3c0922e99db393f191bd8799e2797a66d55
trrr <onegalgo@gmail.com>
2014-02-07 13:33:59 +0400

    Facebook fetcher tests 2.0

--- spec/workers/posts_fetcher_spec.rb
+++ spec/workers/posts_fetcher_spec.rb
@@ -1,50 +1,84 @@
 require 'spec_helper'
 require 'json'
-require 'koala'
 
 describe PostsFetcher do
-  let(:token) {Koala::Facebook::OAuth.new("1419909291585700", "ff5c45bb0c190064ba21b3b9480beb14")}
-  let(:fb_api) {Koala::Facebook::API.new(token.get_app_access_token)}
-  let(:data) {fb_api.get_connections("nokia", 'posts')}
-  let (:raw_post) {JSON.parse(File.read("spec/workers/post.json"))}
+  let(:data) {JSON.parse(File.read("spec/response.json"))}
   let(:posts) {PostsFetcher.serialize_posts(data)}
   let(:author) {Author.create(profile: "nokia")}
 
-
   it "works" do
-    pending "Stub the network!"
-    pending "RIght now it's more of a smoke test, i.e. - it doesn't exlode when i run it"
+    pending "If some test fail, it's probably because of shitty heroku fix. Just turn off author ater_save"
   end
 
-  it "serializes posts" do 
-    serialized_data = PostsFetcher.serialize_posts(data)
-    expect(serialized_data.class).to be Array
-    expect(serialized_data.count).to be > 0
+  context "fetching posts from facebook (hitting the network here!)" do
+
+    before {Author.any_instance.stub(:profile).and_return('nokia')}
+
+    it "fetchs posts of given author from facebook" do
+      pending "uncomment when needed (you'll have to have the right facebook tokens)"
+      # expect(PostsFetcher.fetch_posts(Author.new).count).to be > 5
+    end
   end
 
-  it "extracts data from raw json" do
-    post = PostsFetcher.extract_post_from_raw_post(raw_post)
-    expect(post.message).not_to be_nil
-    expect(post.post_id).to eq raw_post["id"]
+  context "serializing data and droping posts with empty messages" do
+
+    let(:serialized_data) {PostsFetcher.serialize_posts(data)}
+
+    it "serializes posts (extracts data what we need)" do 
+      expect(serialized_data.count).to eq 18
+    end
+
+    it "drops posts with empty messages" do 
+      expect(serialized_data.map(&:message).compact.count).to eq 18
+    end
   end
 
-  it "fetchs posts" do
-    a = Author.stub(:profile).with("nokia")
-    expect{PostsFetcher.fetch_posts(a)}.not_to be_nil
+  context "extracting data from a single facebook post" do
+
+    let(:raw_post) {JSON.parse(File.read("spec/workers/post.json"))}
+    let(:post) {PostsFetcher.extract_post_from_raw_post(raw_post)}
+    let(:message) {"info about our next Tenderlove x dislocated disco party coming soon with very special guests, its been a while <3\nhttp://www.youtube.com/watch?v=okLBcIaVHIA"}
+
+    it "extracts message" do
+      expect(post.message).to eq message
+    end
+
+    it "extracts picture" do
+      expect(post.picture).to eq raw_post["picture"]
+    end
+
+    it "extracts id" do
+      expect(post.post_id).to eq raw_post["id"]
+    end
+
+    it "extracts updated time" do
+      expect(post.updated_time).to eq raw_post["updated_time"]
+    end
+
   end
 
-  context "integration tests" do
+  context "saving new posts to the database" do
+    before {PostsFetcher.check_for_dublications_and_save_posts(posts, author)}
 
+    it "saves all posts to the db" do
+      expect(author.posts.count).to eq 18
+    end
+
+    it "doesn't save posts twice" do
+      PostsFetcher.check_for_dublications_and_save_posts(posts, author)
+      expect(author.posts.count).to eq 18
+    end
+  end
+
+  context "integration tests (Hitting the network!)" do
     it "fetchs and saves posts for given author" do
-      PostsFetcher.fetch_and_save_author_posts(author)
-      expect(author.posts.count).to be > 5
+      pending "uncomment it when needed"
+      # PostsFetcher.fetch_and_save_author_posts(author)
+      # expect(author.posts.count).to be > 5
     end
 
-    it "doesn't save duplicate posts" do
-      PostsFetcher.fetch_and_save_author_posts(author)
-      a = author.posts.count
-      PostsFetcher.fetch_and_save_author_posts(author)
-      expect(author.posts.count).to eq a
-    end
   end
 end
+    
+
+#To save response do: File.open('spec/response.json', 'w'){ |f| f << data.to_json }
