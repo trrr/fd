b7cb74ac1ddc5e538b589c5afacdb0f39b49c25e
trrr <onegalgo@gmail.com>
2014-02-07 14:33:05 +0400

    added spacing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,8 @@
 class CategoriesController < ApplicationController
+
   def index 
     @categories = Category.all
     render json: @categories.as_json(only: [:id, :name, :description])
   end
+  
 end
