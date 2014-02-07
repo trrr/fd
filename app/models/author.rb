6055160df3538f66673e15f8d74be68bc86b6d96
trrr <onegalgo@gmail.com>
2014-02-07 21:01:04 +0400

    removed heroku fix

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,4 @@
 class Author < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   belongs_to :category
-  after_save :fetch_posts
-
-  private 
-    def fetch_posts
-      # Resque.enqueue(PostsFetcher, self.id)
-      PostsFetcher.fetch_and_save_author_posts(self)
-    end
 end
