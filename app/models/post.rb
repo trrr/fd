59240358cc621211698dd4680ec1c3def03218b8
trrr <onegalgo@gmail.com>
2014-02-08 17:48:33 +0400

    using scope

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,12 @@
 class Post < ActiveRecord::Base
   default_scope { order('updated_time DESC') }
+  scope :api_data, -> {select(:id, :message, :picture, :updated_time, :category_id, :author_id)}
   before_save :set_category
   belongs_to :author
 
 
   def self.get_posts_by_params(categories_ids, page = 1, per_page = 10, time, more_or_less)
-    Post.select(:id, :message, :picture, :updated_time, :category_id, :author_id)
-            .where("category_id IN (?) AND updated_time #{more_or_less} (?)", categories_ids, time)
-            .page(page)
-            .per(per_page)
+    Post.api_data.where("category_id IN (?) AND updated_time #{more_or_less} (?)", categories_ids, time).page(page).per(per_page)
   end
 
   private
