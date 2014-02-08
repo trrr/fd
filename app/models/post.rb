6b03b8114e3c7db0db77f68371937bac1fb32dec
trrr <onegalgo@gmail.com>
2014-02-08 18:21:36 +0400

    Using scopes. Made model little more readable.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,14 @@
 class Post < ActiveRecord::Base
   default_scope { order('updated_time DESC') }
   scope :api_data, -> {select(:id, :message, :picture, :updated_time, :category_id, :author_id)}
+  scope :category_in, ->(categories_ids) {where("category_id IN (?)", categories_ids)} 
+  scope :posted_at, ->(before_or_after, time) {where("updated_time #{before_or_after} (?)", time)}
   before_save :set_category
   belongs_to :author
 
 
-  def self.get_posts_by_params(categories_ids, page = 1, per_page = 10, time, more_or_less)
-    Post.api_data.where("category_id IN (?) AND updated_time #{more_or_less} (?)", categories_ids, time).page(page).per(per_page)
+  def self.get_posts_by_params(categories_ids, page = 1, per_page = 10, time, before_or_after)
+    Post.api_data.category_in(categories_ids).posted_at(before_or_after, time).page(page).per(per_page)
   end
 
   private
@@ -14,3 +16,4 @@ class Post < ActiveRecord::Base
       self.category_id = self.author.category_id
     end
 end
+  
