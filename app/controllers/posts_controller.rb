5208b268dc80114b9181727af24b53cf5aa69d55
trrr <onegalgo@gmail.com>
2014-02-10 21:29:29 +0400

    Refactor Posts model and controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,13 @@
 class PostsController < ApplicationController
 
-  # I still don't like this very much. Mostly the fact what we have to perform
-  # search with dummy data when no parameters are given. Will think of it later.
 
   def index
-    @posts = Post.get_posts_by_params(categories_ids, params[:page], params[:per_page], timepoint, up_or_down)
+    if params["id"]
+      timepoint = Post.find(params['id']).updated_time
+      up_or_down = params['behavior'] == 'down' ? '<' : '>'     
+    end
+
+    @posts = Post.get_posts_by_params(params[:tag_id], params[:page], params[:per_page], timepoint, up_or_down)
     render json: @posts
   end
-
-  private
-
-    def timepoint
-      params['action']['id'] ? Post.find(params['action']['id']).updated_time : "2000-01-01T00:00:00.000Z"
-    end
-
-    def up_or_down
-      params['action']['behavior'] == 'down' ? '<' : '>'
-    end
-
-    def categories_ids
-      params[:tag_id] || Category.ids
-    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
   default_scope { order('updated_time DESC') }
   scope :api_data, -> {select(:id, :message, :picture, :updated_time, :category_id, :author_id)}
-  scope :category_in, ->(categories_ids) {where("category_id IN (?)", categories_ids)} 
-  scope :posted_at, ->(before_or_after, time) {where("updated_time #{before_or_after} (?)", time)}
+  scope :category_in, ->(categories_ids) {where("category_id IN (?)", categories_ids) if categories_ids} 
+  scope :posted_at, ->(before_or_after, time) {where("updated_time #{before_or_after} (?)", time) if time && before_or_after}
   before_save :set_category
   belongs_to :author
   validates :author, :message, :updated_time, presence: true
