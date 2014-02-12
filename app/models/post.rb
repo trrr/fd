d329e61e61a743102392c7d04a5ae9f514dc513b
trrr <onegalgo@gmail.com>
2014-02-12 14:34:27 +0400

    Removed page argument

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ActiveRecord::Base
   default_scope { order('updated_time DESC') }
   scope :api_data, -> {select(:id, :message, :picture, :updated_time, :category_id, :author_id)}
-  scope :category_in, ->(categories_ids) {where("category_id IN (?)", categories_ids) if categories_ids} 
+  scope :category_in, ->(categories_ids) {where(category_id: categories_ids) if categories_ids} 
   scope :posted_at, ->(before_or_after, time) {where("updated_time #{before_or_after} (?)", time) if time && before_or_after}
   before_save :set_category
   belongs_to :author
   validates :author, :message, :updated_time, presence: true
 
-  def self.get_posts_by_params(categories_ids, page = 1, per_page = 10, time, before_or_after)
-    Post.api_data.category_in(categories_ids).posted_at(before_or_after, time).page(page).per(per_page)
+  def self.get_posts_by_params(categories_ids, per_page = 10, time, before_or_after)
+    Post.api_data.category_in(categories_ids).posted_at(before_or_after, time).limit(per_page)
   end
 
   private
